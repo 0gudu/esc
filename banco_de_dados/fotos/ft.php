c6f1fadf1d3574ded309a12843cac101b2d6f16a
0gudu <gugag3rr@gmail.com>
2023-06-07 11:36:23 -0300

    fotos de acordo com o perfil e somente uma foto

--- banco_de_dados/fotos/ft.php
+++ banco_de_dados/fotos/ft.php
@@ -1,18 +1,32 @@
-<?php 
+<?php
 include("conecta.php");
+$nome = $_POST["name"];
+$imagem = file_get_contents($_FILES["imagem"]["tmp_name"]);
 
-$comando = $pdo->prepare("INSERT INTO foto(foto) values $_POST["ft"]");
-
+$comando = $pdo->prepare("SELECT COUNT(*) as total FROM foto WHERE nome = :nome");
+$comando->bindParam(":nome", $nome);
 $resultado = $comando->execute();
+$total = $comando->fetchColumn();
 
-$comando = $pdo->prepare("SELECT * FROM foto");
-$resultado = $comando->execute();
-while( $linhas = $comando->fetch() )
-{
-    $dados_imagem = $linhas["foto"];
-    $i = base64_encode($dados_imagem);
-
-    echo(" <img src='data:image/jpeg;base64,$i' width='100px'> <br> <br> ");
+if ($total > 0) {
+    $comando = $pdo->prepare("DELETE FROM foto WHERE nome = :nome");
+    $comando->bindParam(":nome", $nome);
+    $resultado = $comando->execute();
 }
 
+$comando = $pdo->prepare("INSERT INTO foto (foto, nome) VALUES (:foto, :nome)");
+$comando->bindParam(":foto", $imagem, PDO::PARAM_LOB);
+$comando->bindParam(":nome", $nome);
+$resultado = $comando->execute();
+
+$comando = $pdo->prepare("SELECT * FROM foto WHERE nome = :nome");
+$comando->bindParam(":nome", $nome);
+$resultado = $comando->execute();
+
+while ($linhas = $comando->fetch()) {
+    $dados_imagem = $linhas["foto"];
+    echo $linhas["nome"];
+    $i = base64_encode($dados_imagem);
+    echo("<br> <img src='data:image/jpeg;base64,$i' width='100px'> <br> <br> ");
+}
 ?>
