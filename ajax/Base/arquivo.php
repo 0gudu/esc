13fbe823f261a8ee7808dbb84f3d70665819b8a2
0gudu <gugag3rr@gmail.com>
2023-08-10 12:25:36 +0100

    s

--- ajax/Base/arquivo.php
+++ ajax/Base/arquivo.php
@@ -1,28 +1,9 @@
 <?php
+include("conecta.php"); // Assuming this includes the database connection setup
 
-include("conecta.php");
-
-$nome  = $_POST["nome"];  
+$nome = $_POST["nome"];
 $matri = $_POST["matricula"];
 
-$empregados = [];  
-
-$resposta = 
-      [
-         "nome"  => $nome,
-         "matricula" => $matri
-      ];
-
-array_push($empregados, $resposta);
-// Até aqui ficaria dentro do WHILE.
-
-$comando = $pdo ->prepare("INSERT INTO alunos VALUES($matri,'$nome')");
-
-    $resultado = $comando ->execute();
-
-
-// Ao sair do WHILE enviamos de volta para a função JavaScript no formato JSON:
-$json_texto = json_encode(["empregados" => $empregados]);
-echo($json_texto);  // Será retornado para dentro do "success" do arquivo index.html
-
+$comando = $pdo->prepare("INSERT INTO alunos VALUES($matri,'$nome')");
+$resultado = $comando->execute();
 ?>
